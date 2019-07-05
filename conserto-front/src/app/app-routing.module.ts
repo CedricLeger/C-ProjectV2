a5f855ce0c5c45d5d479737091e4ba9dca9ba1aa
Stag LDNR <stagldnr@debian-java-ldnr.ldnr.local>
2019-07-05 12:08:08 +0200

    bug routeur

--- conserto-front/src/app/app-routing.module.ts
+++ conserto-front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginUserComponent } from './components/login-user/login-user.component';
 
 const routes: Routes = [];
 
--- conserto-front/src/app/components/login-user/login-user.component.ts
+++ conserto-front/src/app/components/login-user/login-user.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Form, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-login-user',
-  templateUrl: './login-user.component.html',
-  styleUrls: ['./login-user.component.css']
+  selector: "app-login-user",
+  templateUrl: "./login-user.component.html",
+  styleUrls: ["./login-user.component.css"]
 })
 export class LoginUserComponent implements OnInit {
-  constructor() {}
+  public loginFormGroup: FormGroup;
 
-  ngOnInit() {}
-  onClickSubmit(formData) {
-    alert('Votre email est:' + formData.email);
+  constructor(private routeur: Router, private _formBuilder: FormBuilder) {
+
   }
+
+  ngOnInit() {
+    this.loginFormGroup = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginFormGroup.get('password'));
+  }
+
+  onClickSubmit(formData) {
+    // this.routeur.navigate(/)
+    // alert("Votre email est:" + formData.email);
+  }
+
+
 }
